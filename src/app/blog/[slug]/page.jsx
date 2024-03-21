769c4259be6d29f019a762ef29f11da19f623a39
birdbir <ironses693@gmail.com>
2024-03-21 23:29:16 +0900

    Hi

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -25,22 +25,21 @@ const SinglePostPage = async({params}) => {
   console.log(post)
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image className={styles.img} src="https://images.pexels.com/photos/20195760/pexels-photo-20195760.jpeg" alt="" fill/>
-      </div>
+      {post.img && <div className={styles.imgContainer}>
+        <Image className={styles.img} src={post.img} alt="" fill/>
+      </div>}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post?.title}</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image className={styles.avatar} src="" alt="" width={50} height={50}/>
           {post && (<Suspense fallback={<div>Loading...</div>}> 
           <PostUser userId = {post.userId}/>
           </Suspense>)}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>01.01.2024</span>
+            <span className={styles.detailValue}>{post.createdAt.toString().slice(4, 16)}</span>
           </div>
         </div>
-        <div className={styles.content}>{post?.body}</div>
+        <div className={styles.content}>{post.desc}</div>
       </div>
     </div>
   )
--- src/conponets/postUser/postUser.jsx
+++ src/conponets/postUser/postUser.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './postUser.module.css'
 import { getUser } from '@/lib/data'
+import Image from 'next/image'
+
 
 // const getData = async(userId) => {
 //     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {cashe:"no-store"})
@@ -21,8 +23,11 @@ const PostUser = async({userId}) => {
   const user = await getUser(userId)
   return (
     <div className={styles.container}>
-        <span className={styles.title}>Author</span>
-        <span className={styles.username}>{user.username}</span>
+        <Image className={styles.avatar} src={user.img ? user.img:"/noavatar.png"} alt="" width={50} height={50}/>
+        <div className={styles.texts}>        
+          <span className={styles.title}>Author</span>
+          <span className={styles.username}>{user.username}</span>
+        </div>
     </div>
   )
 }
